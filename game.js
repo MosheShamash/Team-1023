0675aa0b95e72684601496b3560ccffa97d483c4
Moshe Shamash <mshamash@fb.com>
2018-11-16 15:34:11 +0000

    created index.html file as well as game.js file used to instantiate the objects at the beginning of the game - moved into repo

---
+++ game.js
@@ -0,0 +1,8 @@
+x + 10
+var mike = new Character("Mike")
+var postOnline = new Event("Post Online", scenario, 50, 100)
+var playGame = new Event("Play Game", scenario, 60, 100)
+var socialNetwork = new Event("Social Network", scenario, 70, 100)
+var spendTimeOffline = new Event("Spend Time Offline", scenario, 80, 100)
+var homework = new Event("Homework", scenario, 90, 100)
+var postOnlineScenario = new Scenario(postOnlineScenarioText, postOnlineScenarioOption1, postOnlineScenarioOption2, postOnlineScenarioOption3)
---
+++ longVariables.js
@@ -0,0 +1,4 @@
+var postOnlineScenarioText = "You notice your friend has a picture of both of you  in your school uniform on their profile picture on Facebook and has also shared their telephone number."
+var postOnlineScenarioOption1 = "Tag yourself in the photo"
+var postOnlineScenarioOption2 = "Do nothing"
+var postOnlineScenarioOption3 = "Warn them about this being dangerous"
