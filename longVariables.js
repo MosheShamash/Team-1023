7f1efb208bc1d75b9efe43044c5e838f9ea37b40
Moshe Shamash <mshamash@fb.com>
2018-11-19 10:47:33 +0000

    Created variables for scenario text and options

--- longVariables.js
+++ longVariables.js
@@ -1,4 +1,9 @@
 var postOnlineScenarioText = "You notice your friend has a picture of both of you  in your school uniform on their profile picture on Facebook and has also shared their telephone number."
-var postOnlineScenarioOption1 = "Tag yourself in the photo"
-var postOnlineScenarioOption2 = "Do nothing"
-var postOnlineScenarioOption3 = "Warn them about this being dangerous"
+var postOnlineScenarioOption1 = {"Tag yourself in the photo": }
+var postOnlineScenarioOption2 = {"Do nothing"}
+var postOnlineScenarioOption3 = {"Warn them about this being dangerous"}
+
+var playGameScenarioText = "You’re playing a game online, and all of a sudden a pop-up comes up saying you’ve won an iPhone X. What do you do?"
+var playGameScenarioOption1 = "Click on the pop-up - christmas has come early!"
+var playGameScenarioOption2 = "Keep playing - you’re about to get a high score"
+var playGameScenarioOption3 = "Close the window - it’s probably a scam and could give your computer a virus."
